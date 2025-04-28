4b97877951acae1f9aca23ded797ddb7e4d79b41
Sergey Litvinov <slitvinov@gmail.com>
2025-04-28 16:26:27 +0200

    typo

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICE=0 python -m coverage run --parallel-mode baroclinic_instability.py &
-CUDA_VISIBLE_DEVICE=1 python -m coverage run --parallel-mode held_suarez.py &
-CUDA_VISIBLE_DEVICE=2 python -m coverage run --parallel-mode weather_forecast_on_era5.py &
+CUDA_VISIBLE_DEVICES=0 python -m coverage run --parallel-mode baroclinic_instability.py &
+CUDA_VISIBLE_DEVICES=1 python -m coverage run --parallel-mode held_suarez.py &
+CUDA_VISIBLE_DEVICES=2 python -m coverage run --parallel-mode weather_forecast_on_era5.py &
 wait
 
 python -m coverage combine

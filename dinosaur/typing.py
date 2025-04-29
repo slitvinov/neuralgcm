ef7b3ff8377d310f21dca729c444d6bfb371a9d5
Sergey Litvinov <slitvinov@gmail.com>
2025-04-29 11:26:50 +0200

    All any

--- dinosaur/typing.py
+++ dinosaur/typing.py
@@ -1,8 +1,3 @@
-from typing import Any, TypeVar
-from dinosaur import scales
-import jax.numpy as jnp
-import numpy as np
-import tree_math
-
-Array = Union[np.ndarray, jnp.ndarray]
-State = TypeVar("State")
+from typing import Any
+Array = Any
+State = Any

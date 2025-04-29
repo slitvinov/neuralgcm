3d442bce6ed28e8cc76744d352d7e454dcb40938
Sergey Litvinov <slitvinov@gmail.com>
2025-04-29 11:02:21 +0200

    up

--- dinosaur/typing.py
+++ dinosaur/typing.py
@@ -6,17 +6,10 @@ import numpy as np
 import tree_math
 
 Array = Union[np.ndarray, jnp.ndarray]
-ArrayOrArrayTuple = Union[Array, tuple[Array, ...]]
 Numeric = Union[float, int, Array]
 Quantity = scales.Quantity
 PRNGKeyArray = Any
 PyTreeState = TypeVar('PyTreeState')
 Pytree = Any
-PyTreeMemory = Pytree
-PyTreeDiagnostics = Pytree
-AuxFeatures = dict[str, Any]
-DataState = dict[str, Any]
-ForcingData = dict[str, Any]
 State = TypeVar('State')
-Forcing = Pytree
 Params = Union[Mapping[str, Mapping[str, Array]], None]

c9c50ae335d613b6826f539507ab6b0865470234
Sergey Litvinov <slitvinov@gmail.com>
2025-04-30 12:15:54 +0200

    mv baroclinic_instability

--- baroclinic_instability.py
+++ baroclinic_instability.py
@@ -6,11 +6,182 @@ import xarray
 import di
 
 units = di.units
+
+
+def steady_state_jw(
+    coords,
+    u0=35.0 * units.m / units.s,
+    p0=1e5 * units.pascal,
+    t0=288.0 * units.degK,
+    delta_t=4.8e5 * units.degK,
+    gamma=0.005 * units.degK / units.m,
+    sigma_tropo: float = 0.2,
+    sigma0: float = 0.252,
+):
+    u0 = di.DEFAULT_SCALE.nondimensionalize(u0)
+    t0 = di.DEFAULT_SCALE.nondimensionalize(t0)
+    delta_t = di.DEFAULT_SCALE.nondimensionalize(delta_t)
+    p0 = di.DEFAULT_SCALE.nondimensionalize(p0)
+    gamma = di.DEFAULT_SCALE.nondimensionalize(gamma)
+    a = di.radius
+    g = di.gravity_acceleration
+    r_gas = di.ideal_gas_constant
+    omega = di.angular_velocity
+
+    def _get_reference_temperature(sigma):
+        top_mean_t = t0 * sigma**(r_gas * gamma / g)
+        if sigma < sigma_tropo:
+            return top_mean_t + delta_t * (sigma_tropo - sigma)**5
+        else:
+            return top_mean_t
+
+    def _get_reference_geopotential(sigma):
+        top_mean_potential = (t0 * g / gamma) * (1 -
+                                                 sigma**(r_gas * gamma / g))
+        if sigma < sigma_tropo:
+            return top_mean_potential - r_gas * delta_t * (
+                (np.log(sigma / sigma_tropo) + 137 / 60) * sigma_tropo**5 -
+                5 * sigma * sigma_tropo**4 + 5 * (sigma**2) *
+                (sigma_tropo**3) - (10 / 3) * (sigma_tropo**2) * sigma**3 +
+                (5 / 4) * sigma_tropo * sigma**4 - (sigma**5) / 5)
+        else:
+            return top_mean_potential
+
+    def _get_geopotential(lat, lon, sigma):
+        del lon
+        sigma_nu = (sigma - sigma0) * np.pi / 2
+        return _get_reference_geopotential(
+            sigma) + u0 * np.cos(sigma_nu)**1.5 * (
+                ((-2 * np.sin(lat)**6 *
+                  (np.cos(lat)**2 + 1 / 3) + 10 / 63) * u0 * np.cos(sigma_nu)**
+                 (3 / 2)) +
+                ((1.6 * (np.cos(lat)**3) *
+                  (np.sin(lat)**2 + 2 / 3) - np.pi / 4) * a * omega))
+
+    def _get_temperature_variation(lat, lon, sigma):
+        del lon
+        sigma_nu = (sigma - sigma0) * np.pi / 2
+        cos_𝜎ν = np.cos(sigma_nu)
+        sin_𝜎ν = np.sin(sigma_nu)
+        return (0.75 * (sigma * np.pi * u0 / r_gas) * sin_𝜎ν *
+                np.sqrt(cos_𝜎ν) *
+                (((-2 * (np.cos(lat)**2 + 1 / 3) * np.sin(lat)**6 + 10 / 63) *
+                  2 * u0 * cos_𝜎ν**(3 / 2)) +
+                 ((1.6 * (np.cos(lat)**3) *
+                   (np.sin(lat)**2 + 2 / 3) - np.pi / 4) * a * omega)))
+
+    def _get_vorticity(lat, lon, sigma):
+        del lon
+        sigma_nu = (sigma - sigma0) * np.pi / 2
+        return ((-4 * u0 / a) * (np.cos(sigma_nu)**(3 / 2)) * np.sin(lat) *
+                np.cos(lat) * (2 - 5 * np.sin(lat)**2))
+
+    def _get_surface_pressure(
+        lat,
+        lon,
+    ):
+        del lon
+        return p0 * np.ones(lat.shape)[np.newaxis, ...]
+
+    lon, sin_lat = coords.horizontal.nodal_mesh
+    lat = np.arcsin(sin_lat)
+
+    def initial_state_fn(rng_key=None):
+        del rng_key
+        nodal_vorticity = np.stack([
+            _get_vorticity(lat, lon, sigma)
+            for sigma in coords.vertical.centers
+        ])
+        modal_vorticity = coords.horizontal.to_modal(nodal_vorticity)
+        nodal_temperature_variation = np.stack([
+            _get_temperature_variation(lat, lon, sigma)
+            for sigma in coords.vertical.centers
+        ])
+        log_nodal_surface_pressure = np.log(_get_surface_pressure(lat, lon))
+        state = di.State(
+            vorticity=modal_vorticity,
+            divergence=np.zeros_like(modal_vorticity),
+            temperature_variation=coords.horizontal.to_modal(
+                nodal_temperature_variation),
+            log_surface_pressure=coords.horizontal.to_modal(
+                log_nodal_surface_pressure),
+        )
+        return state
+
+    orography = _get_geopotential(lat, lon, 1.0) / g
+    geopotential = np.stack([
+        _get_geopotential(lat, lon, sigma) for sigma in coords.vertical.centers
+    ])
+    reference_temperatures = np.stack([
+        _get_reference_temperature(sigma) for sigma in coords.vertical.centers
+    ])
+    aux_features = {
+        "geopotential": geopotential,
+        "orography": orography,
+        "ref_temperatures": reference_temperatures,
+    }
+    return initial_state_fn, aux_features
+
+
+def baroclinic_perturbation_jw(
+    coords,
+    u_perturb=1.0 * units.m / units.s,
+    lon_location=np.pi / 9,
+    lat_location=2 * np.pi / 9,
+    perturbation_radius=0.1,
+):
+    u_p = DEFAULT_SCALE.nondimensionalize(u_perturb)
+    a = radius
+
+    def _get_vorticity_perturbation(lat, lon, sigma):
+        del sigma
+        x = np.sin(lat_location) * np.sin(lat) + np.cos(lat_location) * np.cos(
+            lat) * np.cos(lon - lon_location)
+        r = a * np.arccos(x)
+        R = a * perturbation_radius
+        return ((u_p / a) * np.exp(-((r / R)**2)) *
+                (np.tan(lat) - (2 * ((a / R)**2) * np.arccos(x)) *
+                 (np.sin(lat_location) * np.cos(lat) - np.cos(lat_location) *
+                  np.sin(lat) * np.cos(lon - lon_location)) /
+                 (np.sqrt(1 - x**2))))
+
+    def _get_divergence_perturbation(lat, lon, sigma):
+        del sigma
+        x = np.sin(lat_location) * np.sin(lat) + np.cos(lat_location) * np.cos(
+            lat) * np.cos(lon - lon_location)
+        r = a * np.arccos(x)
+        R = a * perturbation_radius
+        return ((-2 * u_p * a /
+                 (R**2)) * np.exp(-((r / R)**2)) * np.arccos(x) *
+                ((np.cos(lat_location) * np.sin(lon - lon_location)) /
+                 (np.sqrt(1 - x**2))))
+
+    lon, sin_lat = coords.horizontal.nodal_mesh
+    lat = np.arcsin(sin_lat)
+    nodal_vorticity = np.stack([
+        _get_vorticity_perturbation(lat, lon, sigma)
+        for sigma in coords.vertical.centers
+    ])
+    nodal_divergence = np.stack([
+        _get_divergence_perturbation(lat, lon, sigma)
+        for sigma in coords.vertical.centers
+    ])
+    modal_vorticity = coords.horizontal.to_modal(nodal_vorticity)
+    modal_divergence = coords.horizontal.to_modal(nodal_divergence)
+    state = State(
+        vorticity=modal_vorticity,
+        divergence=modal_divergence,
+        temperature_variation=np.zeros_like(modal_vorticity),
+        log_surface_pressure=np.zeros_like(modal_vorticity[:1, ...]),
+    )
+    return state
+
+
 layers = 24
 grid = di.Grid.T42()
 vertical_grid = di.SigmaCoordinates.equidistant(layers)
 coords = di.CoordinateSystem(grid, vertical_grid)
-initial_state_fn, aux_features = di.steady_state_jw(coords)
+initial_state_fn, aux_features = steady_state_jw(coords)
 steady_state = initial_state_fn()
 ref_temps = aux_features["ref_temperatures"]
 orography = di.truncated_modal_orography(aux_features["orography"], coords)
@@ -27,13 +198,10 @@ u, v = di.vor_div_to_uv_nodal(grid, steady_state.vorticity,
                               steady_state.divergence)
 steady_state_dict.update({"u": u, "v": v, "z_surf": orography})
 f0 = di.maybe_to_nodal(steady_state_dict, coords=coords)
-x0 = di.data_to_xarray(f0,
-                                     coords=coords,
-                                     times=None)
+x0 = di.data_to_xarray(f0, coords=coords, times=None)
 temperature = di.temperature_variation_to_absolute(
     x0.temperature_variation.data, ref_temps)
-x0 = x0.assign(
-    temperature=(x0.temperature_variation.dims, temperature))
+x0 = x0.assign(temperature=(x0.temperature_variation.dims, temperature))
 phi = x0["z_surf"] * di.gravity_acceleration
 phi_si = dimensionalize(phi, units.m**2 / units.s**2)
 phi_si.isel(lon=0).plot(x="lat")
@@ -85,24 +253,19 @@ u, v = di.vor_div_to_uv_nodal(grid, trajectory.vorticity,
                               trajectory.divergence)
 trajectory_dict.update({"u": u, "v": v})
 f1 = di.maybe_to_nodal(trajectory_dict, coords=coords)
-x1 = di.data_to_xarray(f1,
-                                  coords=coords,
-                                  times=times)
-x1["surface_pressure"] = np.exp(
-    x1.log_surface_pressure[:, 0, :, :])
+x1 = di.data_to_xarray(f1, coords=coords, times=times)
+x1["surface_pressure"] = np.exp(x1.log_surface_pressure[:, 0, :, :])
 temperature = di.temperature_variation_to_absolute(
     x1.temperature_variation.data, ref_temps)
-x1 = x1.assign(
-    temperature=(x1.temperature_variation.dims, temperature))
+x1 = x1.assign(temperature=(x1.temperature_variation.dims, temperature))
 data_array = x1["vorticity"]
 data_array.isel(lon=0).thin(time=12).plot.contour(x="lat",
                                                   y="level",
                                                   col="time")
 ax = plt.gca()
 ax.set_ylim((1, 0))
-data_array = x1[
-    "surface_pressure"] / di.DEFAULT_SCALE.nondimensionalize(
-        1e5 * units.pascal)
+data_array = x1["surface_pressure"] / di.DEFAULT_SCALE.nondimensionalize(
+    1e5 * units.pascal)
 data_array.max(["lon"]).plot(x="time", hue="lat")
 ax = plt.gca()
 ax.legend().remove()
@@ -140,18 +303,13 @@ u, v = di.vor_div_to_uv_nodal(grid, trajectory.vorticity,
                               trajectory.divergence)
 trajectory_dict.update({"u": u, "v": v})
 f1 = di.maybe_to_nodal(trajectory_dict, coords=coords)
-x1 = di.data_to_xarray(f1,
-                                  coords=coords,
-                                  times=times)
-x1["surface_pressure"] = np.exp(
-    x1.log_surface_pressure[:, 0, :, :])
+x1 = di.data_to_xarray(f1, coords=coords, times=times)
+x1["surface_pressure"] = np.exp(x1.log_surface_pressure[:, 0, :, :])
 temperature = di.temperature_variation_to_absolute(
     x1.temperature_variation.data, ref_temps)
-x1 = x1.assign(
-    temperature=(x1.temperature_variation.dims, temperature))
-data_array = x1[
-    "surface_pressure"] / di.DEFAULT_SCALE.nondimensionalize(
-        1e5 * units.pascal)
+x1 = x1.assign(temperature=(x1.temperature_variation.dims, temperature))
+data_array = x1["surface_pressure"] / di.DEFAULT_SCALE.nondimensionalize(
+    1e5 * units.pascal)
 levels = [(992 + 2 * i) / 1000 for i in range(8)]
 data_array.sel({
     "lat":
@@ -168,9 +326,8 @@ fig = plt.gcf()
 fig.set_figwidth(10)
 plt.savefig("b.07.png")
 plt.close()
-data_array = x1[
-    "surface_pressure"] / di.DEFAULT_SCALE.nondimensionalize(
-        1e5 * units.pascal)
+data_array = x1["surface_pressure"] / di.DEFAULT_SCALE.nondimensionalize(
+    1e5 * units.pascal)
 levels = [(930 + 10 * i) / 1000 for i in range(10)]
 (data_array.sel({
     "lat":

98a98ca29deabd4b0ea7aa50bf02628b5df565ba
Sergey Litvinov <slitvinov@gmail.com>
2025-06-04 14:08:37 +0200

    TF_CPP_MIN_LOG_LEVEL

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
+TF_CPP_MIN_LOG_LEVEL=3 export TF_CPP_MIN_LOG_LEVEL
 CUDA_VISIBLE_DEVICES=0 python -m coverage run --parallel-mode baroclinic_instability.py &
 CUDA_VISIBLE_DEVICES=1 python -m coverage run --parallel-mode held_suarez.py &
 CUDA_VISIBLE_DEVICES=2 python -m coverage run --parallel-mode weather_forecast_on_era5.py &

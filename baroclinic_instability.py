051a952699cf25cd29ea555127e7661802ed53fb
Sergey Litvinov <slitvinov@gmail.com>
2025-06-04 13:23:28 +0200

    minor

--- baroclinic_instability.py
+++ baroclinic_instability.py
@@ -222,9 +222,9 @@ u, v = di.vor_div_to_uv_nodal(grid, trajectory.vorticity,
                               trajectory.divergence)
 trajectory_dict.update({"u": u, "v": v})
 f1 = di.maybe_to_nodal(trajectory_dict, coords=coords)
+temperature = di.temperature_variation_to_absolute(f1["temperature_variation"],
+                                                   ref_temps)
 levels = [(220 + 10 * i) for i in range(10)]
-plt.contourf(f1["temperature_variation"][119, 22, :, :],
-             levels=levels,
-             cmap=plt.cm.Spectral_r)
+plt.contourf(temperature[119, 22, :, :], levels=levels, cmap=plt.cm.Spectral_r)
 plt.savefig("b.09.png")
 plt.close()

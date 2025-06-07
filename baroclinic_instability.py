44f7266eb20f39aacf310f455f80e7d268eb9483
Sergey Litvinov <slitvinov@gmail.com>
2025-06-07 09:59:46 -0400

    rm steady_state_jw

--- baroclinic_instability.py
+++ baroclinic_instability.py
@@ -10,116 +10,79 @@ def temperature_variation_to_absolute(temperature_variation, ref_temperature):
     return temperature_variation + ref_temperature[:, np.newaxis, np.newaxis]
 
 
-def steady_state_jw(
-    coords,
-    u0=35.0 * units.m / units.s,
-    p0=1e5 * units.pascal,
-    t0=288.0 * units.degK,
-    delta_t=4.8e5 * units.degK,
-    gamma=0.005 * units.degK / units.m,
-    sigma_tropo: float = 0.2,
-    sigma0: float = 0.252,
-):
-    u0 = di.DEFAULT_SCALE.nondimensionalize(u0)
-    t0 = di.DEFAULT_SCALE.nondimensionalize(t0)
-    delta_t = di.DEFAULT_SCALE.nondimensionalize(delta_t)
-    p0 = di.DEFAULT_SCALE.nondimensionalize(p0)
-    gamma = di.DEFAULT_SCALE.nondimensionalize(gamma)
-    g = di.gravity_acceleration
-    r_gas = di.ideal_gas_constant
+def get_reference_temperature(sigma):
+    top_mean_t = t0 * sigma**(r_gas * gamma / g)
+    if sigma < sigma_tropo:
+        return top_mean_t + delta_t * (sigma_tropo - sigma)**5
+    else:
+        return top_mean_t
 
-    def _get_reference_temperature(sigma):
-        top_mean_t = t0 * sigma**(r_gas * gamma / g)
-        if sigma < sigma_tropo:
-            return top_mean_t + delta_t * (sigma_tropo - sigma)**5
-        else:
-            return top_mean_t
 
-    def _get_reference_geopotential(sigma):
-        top_mean_potential = (t0 * g / gamma) * (1 -
-                                                 sigma**(r_gas * gamma / g))
-        if sigma < sigma_tropo:
-            return top_mean_potential - r_gas * delta_t * (
-                (np.log(sigma / sigma_tropo) + 137 / 60) * sigma_tropo**5 -
-                5 * sigma * sigma_tropo**4 + 5 * (sigma**2) *
-                (sigma_tropo**3) - (10 / 3) * (sigma_tropo**2) * sigma**3 +
-                (5 / 4) * sigma_tropo * sigma**4 - (sigma**5) / 5)
-        else:
-            return top_mean_potential
+def get_reference_geopotential(sigma):
+    top_mean_potential = (t0 * g / gamma) * (1 - sigma**(r_gas * gamma / g))
+    if sigma < sigma_tropo:
+        return top_mean_potential - r_gas * delta_t * (
+            (np.log(sigma / sigma_tropo) + 137 / 60) * sigma_tropo**5 -
+            5 * sigma * sigma_tropo**4 + 5 * (sigma**2) * (sigma_tropo**3) -
+            (10 / 3) * (sigma_tropo**2) * sigma**3 +
+            (5 / 4) * sigma_tropo * sigma**4 - (sigma**5) / 5)
+    else:
+        return top_mean_potential
 
-    def _get_geopotential(lat, lon, sigma):
-        del lon
-        sigma_nu = (sigma - sigma0) * np.pi / 2
-        return _get_reference_geopotential(
-            sigma) + u0 * np.cos(sigma_nu)**1.5 * (
-                ((-2 * np.sin(lat)**6 *
-                  (np.cos(lat)**2 + 1 / 3) + 10 / 63) * u0 * np.cos(sigma_nu)**
-                 (3 / 2)) + ((1.6 * (np.cos(lat)**3) *
-                              (np.sin(lat)**2 + 2 / 3) - np.pi / 4) * 0.5))
 
-    def _get_temperature_variation(lat, lon, sigma):
-        del lon
-        sigma_nu = (sigma - sigma0) * np.pi / 2
-        cos_𝜎ν = np.cos(sigma_nu)
-        sin_𝜎ν = np.sin(sigma_nu)
-        return (0.75 * (sigma * np.pi * u0 / r_gas) * sin_𝜎ν *
-                np.sqrt(cos_𝜎ν) *
-                (((-2 * (np.cos(lat)**2 + 1 / 3) * np.sin(lat)**6 + 10 / 63) *
-                  2 * u0 * cos_𝜎ν**(3 / 2)) +
-                 ((1.6 * (np.cos(lat)**3) *
-                   (np.sin(lat)**2 + 2 / 3) - np.pi / 4) * 0.5)))
+def get_geopotential(lat, lon, sigma):
+    del lon
+    sigma_nu = (sigma - sigma0) * np.pi / 2
+    return get_reference_geopotential(sigma) + u0 * np.cos(sigma_nu)**1.5 * (
+        ((-2 * np.sin(lat)**6 *
+          (np.cos(lat)**2 + 1 / 3) + 10 / 63) * u0 * np.cos(sigma_nu)**
+         (3 / 2)) + ((1.6 * (np.cos(lat)**3) *
+                      (np.sin(lat)**2 + 2 / 3) - np.pi / 4) * 0.5))
 
-    def _get_vorticity(lat, lon, sigma):
-        del lon
-        sigma_nu = (sigma - sigma0) * np.pi / 2
-        return ((-4 * u0) * (np.cos(sigma_nu)**(3 / 2)) * np.sin(lat) *
-                np.cos(lat) * (2 - 5 * np.sin(lat)**2))
 
-    def _get_surface_pressure(
-        lat,
-        lon,
-    ):
-        del lon
-        return p0 * np.ones(lat.shape)[np.newaxis, ...]
+def get_temperature_variation(lat, lon, sigma):
+    del lon
+    sigma_nu = (sigma - sigma0) * np.pi / 2
+    cos_𝜎ν = np.cos(sigma_nu)
+    sin_𝜎ν = np.sin(sigma_nu)
+    return (0.75 * (sigma * np.pi * u0 / r_gas) * sin_𝜎ν * np.sqrt(cos_𝜎ν) *
+            (((-2 * (np.cos(lat)**2 + 1 / 3) * np.sin(lat)**6 + 10 / 63) * 2 *
+              u0 * cos_𝜎ν**(3 / 2)) +
+             ((1.6 * (np.cos(lat)**3) *
+               (np.sin(lat)**2 + 2 / 3) - np.pi / 4) * 0.5)))
 
-    lon, sin_lat = coords.horizontal.nodal_mesh
-    lat = np.arcsin(sin_lat)
 
-    def initial_state_fn(rng_key=None):
-        del rng_key
-        nodal_vorticity = np.stack([
-            _get_vorticity(lat, lon, sigma)
-            for sigma in coords.vertical.centers
-        ])
-        modal_vorticity = coords.horizontal.to_modal(nodal_vorticity)
-        nodal_temperature_variation = np.stack([
-            _get_temperature_variation(lat, lon, sigma)
-            for sigma in coords.vertical.centers
-        ])
-        log_nodal_surface_pressure = np.log(_get_surface_pressure(lat, lon))
-        state = di.State(
-            vorticity=modal_vorticity,
-            divergence=np.zeros_like(modal_vorticity),
-            temperature_variation=coords.horizontal.to_modal(
-                nodal_temperature_variation),
-            log_surface_pressure=coords.horizontal.to_modal(
-                log_nodal_surface_pressure),
-        )
-        return state
+def get_vorticity(lat, lon, sigma):
+    del lon
+    sigma_nu = (sigma - sigma0) * np.pi / 2
+    return ((-4 * u0) * (np.cos(sigma_nu)**(3 / 2)) * np.sin(lat) *
+            np.cos(lat) * (2 - 5 * np.sin(lat)**2))
 
-    orography = _get_geopotential(lat, lon, 1.0) / g
-    geopotential = np.stack([
-        _get_geopotential(lat, lon, sigma) for sigma in coords.vertical.centers
+
+def get_surface_pressure(lat, lon):
+    del lon
+    return p0 * np.ones(lat.shape)[np.newaxis, ...]
+
+
+def initial_state_fn(rng_key=None):
+    del rng_key
+    nodal_vorticity = np.stack(
+        [get_vorticity(lat, lon, sigma) for sigma in coords.vertical.centers])
+    modal_vorticity = coords.horizontal.to_modal(nodal_vorticity)
+    nodal_temperature_variation = np.stack([
+        get_temperature_variation(lat, lon, sigma)
+        for sigma in coords.vertical.centers
     ])
-    reference_temperatures = np.stack([
-        _get_reference_temperature(sigma) for sigma in coords.vertical.centers
-    ])
-    aux_features = {
-        "geopotential": geopotential,
-        "orography": orography,
-        "ref_temperatures": reference_temperatures,
-    }
-    return initial_state_fn, aux_features
+    log_nodal_surface_pressure = np.log(get_surface_pressure(lat, lon))
+    state = di.State(
+        vorticity=modal_vorticity,
+        divergence=np.zeros_like(modal_vorticity),
+        temperature_variation=coords.horizontal.to_modal(
+            nodal_temperature_variation),
+        log_surface_pressure=coords.horizontal.to_modal(
+            log_nodal_surface_pressure),
+    )
+    return state
 
 
 def baroclinic_perturbation_jw(
@@ -131,7 +94,7 @@ def baroclinic_perturbation_jw(
 ):
     u_p = di.DEFAULT_SCALE.nondimensionalize(u_perturb)
 
-    def _get_vorticity_perturbation(lat, lon, sigma):
+    def get_vorticity_perturbation(lat, lon, sigma):
         del sigma
         x = np.sin(lat_location) * np.sin(lat) + np.cos(lat_location) * np.cos(
             lat) * np.cos(lon - lon_location)
@@ -143,7 +106,7 @@ def baroclinic_perturbation_jw(
                   np.sin(lat) * np.cos(lon - lon_location)) /
                  (np.sqrt(1 - x**2))))
 
-    def _get_divergence_perturbation(lat, lon, sigma):
+    def get_divergence_perturbation(lat, lon, sigma):
         del sigma
         x = np.sin(lat_location) * np.sin(lat) + np.cos(lat_location) * np.cos(
             lat) * np.cos(lon - lon_location)
@@ -156,11 +119,11 @@ def baroclinic_perturbation_jw(
     lon, sin_lat = coords.horizontal.nodal_mesh
     lat = np.arcsin(sin_lat)
     nodal_vorticity = np.stack([
-        _get_vorticity_perturbation(lat, lon, sigma)
+        get_vorticity_perturbation(lat, lon, sigma)
         for sigma in coords.vertical.centers
     ])
     nodal_divergence = np.stack([
-        _get_divergence_perturbation(lat, lon, sigma)
+        get_divergence_perturbation(lat, lon, sigma)
         for sigma in coords.vertical.centers
     ])
     modal_vorticity = coords.horizontal.to_modal(nodal_vorticity)
@@ -181,8 +144,35 @@ grid = di.Grid(longitude_wavenumbers=22,
                latitude_nodes=32)
 vertical_grid = di.SigmaCoordinates(
     np.linspace(0, 1, layers + 1, dtype=np.float32))
+
+u0 = 35.0 * units.m / units.s
+p0 = 1e5 * units.pascal
+t0 = 288.0 * units.degK
+delta_t = 4.8e5 * units.degK
+gamma = 0.005 * units.degK / units.m
+sigma_tropo = 0.2
+sigma0 = 0.252
 coords = di.CoordinateSystem(grid, vertical_grid)
-initial_state_fn, aux_features = steady_state_jw(coords)
+u0 = di.DEFAULT_SCALE.nondimensionalize(u0)
+t0 = di.DEFAULT_SCALE.nondimensionalize(t0)
+delta_t = di.DEFAULT_SCALE.nondimensionalize(delta_t)
+p0 = di.DEFAULT_SCALE.nondimensionalize(p0)
+gamma = di.DEFAULT_SCALE.nondimensionalize(gamma)
+g = di.gravity_acceleration
+r_gas = di.ideal_gas_constant
+lon, sin_lat = coords.horizontal.nodal_mesh
+lat = np.arcsin(sin_lat)
+orography = get_geopotential(lat, lon, 1.0) / g
+geopotential = np.stack(
+    [get_geopotential(lat, lon, sigma) for sigma in coords.vertical.centers])
+reference_temperatures = np.stack(
+    [get_reference_temperature(sigma) for sigma in coords.vertical.centers])
+aux_features = {
+    "geopotential": geopotential,
+    "orography": orography,
+    "ref_temperatures": reference_temperatures,
+}
+
 steady_state = initial_state_fn()
 ref_temps = aux_features["ref_temperatures"]
 orography = di.truncated_modal_orography(aux_features["orography"], coords)

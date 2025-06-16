83c994b5bd8e0210a1c64c3ccfd5616e6b5c48ac
Sergey Litvinov <slitvinov@gmail.com>
2025-06-16 12:22:33 +0200

    Revert "/ step_size"
    This reverts commit 16e88e93eb03c9d92f29f92a6241c3c55a33c264.

--- di.py
+++ di.py
@@ -696,14 +696,14 @@ def implicit_inverse(state, step_size):
     row0 = np.concatenate(
         [
             np.broadcast_to(eye, [l, j, k]),
-            np.einsum("l,jk->ljk", lam, geo),
-            r * np.einsum("l,jo->ljo", lam, t),
+            step_size * np.einsum("l,jk->ljk", lam, geo),
+            step_size * r * np.einsum("l,jo->ljo", lam, t),
         ],
         axis=2,
     )
     row1 = np.concatenate(
         [
-            np.broadcast_to(h[np.newaxis], [l, j, k]),
+            step_size * np.broadcast_to(h[np.newaxis], [l, j, k]),
             np.broadcast_to(eye, [l, j, k]),
             np.zeros([l, j, 1]),
         ],
@@ -711,7 +711,7 @@ def implicit_inverse(state, step_size):
     )
     row2 = np.concatenate(
         [
-            np.broadcast_to(thickness, [l, 1, k]),
+            np.broadcast_to(step_size * thickness, [l, 1, k]),
             np.zeros([l, 1, k]),
             np.ones([l, 1, 1]),
         ],
@@ -724,7 +724,7 @@ def implicit_inverse(state, step_size):
     temp = slice(layers, 2 * layers)
     logp = slice(2 * layers, 2 * layers + 1)
 
-    inverse = np.linalg.inv(implicit_matrix) / step_size
+    inverse = np.linalg.inv(implicit_matrix)
     assert not np.isnan(inverse).any()
     inverted_divergence = (
         matvec(inverse[:, div, div], state.divergence) +

581a4ca6cb93319d2f732b6e04eca7580f41053b
Sergey Litvinov <slitvinov@gmail.com>
2025-06-20 11:11:43 +0200

    up

--- weather.py
+++ weather.py
@@ -195,9 +195,8 @@ for var_name in var_names:
     data = ds_nondim_init[var_name].transpose(..., "longitude",
                                               "latitude").data
     if data.ndim == 2:
-        data = data[np.newaxis, ...]
-    else:
         assert 0
+        data = data[np.newaxis, ...]
     model_level_inputs[var_name] = data
 sp_nodal = model_level_inputs.pop("surface_pressure")
 orography_input = model_level_inputs.pop("orography")

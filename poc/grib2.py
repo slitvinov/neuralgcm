df24d14b261f11708b731ef77618b251827709f2
Sergey Litvinov <slitvinov@gmail.com>
2025-06-22 15:32:39 +0200

    lengthPu

--- poc/grib2.py
+++ poc/grib2.py
@@ -7,7 +7,7 @@ magic = indicator[:4]
 assert magic == b'GRIB'
 discipline = indicator[6]
 edition = indicator[7]
-length = indicator[8:]
-length = struct.unpack(">Q", length)
+assert edition == 2
+length, = struct.unpack(">Q", indicator[8:])
 print(f"{discipline=} {edition=} {length=}")
 f.close()

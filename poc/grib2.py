ec309d62ce93228a7241f7841c8e655452dfcbc4
Sergey Litvinov <slitvinov@gmail.com>
2025-06-22 16:33:58 +0200

    up

--- poc/grib2.py
+++ poc/grib2.py
@@ -6,7 +6,7 @@ indicator = f.read(16)
 magic = indicator[:4]
 assert magic == b'GRIB'
 discipline = indicator[6]
-assert edition == 0 # Meteorological Products
+assert discipline == 0 # Meteorological Products
 edition = indicator[7]
 assert edition == 2
 total_length, = struct.unpack(">Q", indicator[8:])

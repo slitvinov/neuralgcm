fd89f9b206c61d6d9ac92636bca0c66cb9fdf97f
Sergey Litvinov <slitvinov@gmail.com>
2025-06-22 15:30:38 +0200

    up

--- poc/grib2.py
+++ poc/grib2.py
@@ -9,7 +9,7 @@ discipline = indicator[6]
 edition = indicator[7]
 length = indicator[8:]
 print(f"{discipline=} {edition=} {length=}")
-for fmt in "LlqQ":
+for fmt in "qQ":
     for b in "<>=":
         print(b + fmt, struct.unpack(b + fmt, length))
 f.close()

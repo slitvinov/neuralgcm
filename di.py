e1679e7016937cd460f5de94f1ffd4b641674424
Sergey Litvinov <slitvinov@gmail.com>
2025-06-23 08:55:21 +0200

    up

--- di.py
+++ di.py
@@ -129,6 +129,7 @@ def centered_difference(x):
 
 
 def cumulative_sigma_integral(x):
+    jax.debug.print("x.ndim:", x.ndim)
     x_axes = range(x.ndim)
     d𝜎 = g.layer_thickness
     d𝜎_axes = [x_axes[-3]]

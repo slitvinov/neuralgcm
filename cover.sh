4fa404a60779d123a7d7ac6039001622775fc3a4
Sergey Litvinov <slitvinov@gmail.com>
2025-06-26 11:57:54 +0200

    run

--- cover.sh
+++ cover.sh
@@ -3,7 +3,7 @@
 TF_CPP_MIN_LOG_LEVEL=3 export TF_CPP_MIN_LOG_LEVEL
 CUDA_VISIBLE_DEVICES=0 COVERAGE_FILE=.coverage.0 python -m coverage run baroclinic.py &
 CUDA_VISIBLE_DEVICES=1 COVERAGE_FILE=.coverage.1 python -m coverage run held_suarez.py &
-CUDA_VISIBLE_DEVICES=2 COVERAGE_FILE=.coverage.2 python -m coverage weather.py &
+CUDA_VISIBLE_DEVICES=2 COVERAGE_FILE=.coverage.2 python -m coverage run weather.py &
 wait
 
 python -m coverage combine

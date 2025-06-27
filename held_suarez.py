dccd50c0aa7faf5cf42283dfad6c28799b219b4d
Sergey Litvinov <slitvinov@gmail.com>
2025-06-27 09:42:29 +0200

    up

--- held_suarez.py
+++ held_suarez.py
@@ -24,8 +24,8 @@ def explicit_terms(state):
     kv_coeff = kf * (np.maximum(0, (di.g.centers - sigma_b) / (1 - sigma_b)))
     kv = kv_coeff[:, np.newaxis, np.newaxis]
     sin_lat, _ = scipy.special.roots_legendre(di.g.latitude_nodes)
-    cos = np.sqrt(1 - sin_lat**2)
-    nodal_velocity_tendency = -kv * u0 / cos**2, -kv * u1 / cos**2
+    cos = 1 - sin_lat**2
+    nodal_velocity_tendency = -kv * u0 / cos2, -kv * u1 / cos2
     nodal_temperature = (
         di.g.reference_temperature[:, np.newaxis, np.newaxis] +
         temperature_variation)

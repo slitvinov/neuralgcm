05be1dad47c8c5b2741a4c7ff6cd78321625d44b
Sergey Litvinov <slitvinov@gmail.com>
2025-06-27 09:06:03 +0200

    up

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TF_CPP_MIN_LOG_LEVEL=3 export TF_CPP_MIN_LOG_LEVEL
-CUDA_VISIBLE_DEVICES=0 python baroclinic.py &
-CUDA_VISIBLE_DEVICES=1 python held_suarez.py &
-CUDA_VISIBLE_DEVICES=2 python weather.py &
+CUDA_VISIBLE_DEVICES=1 python baroclinic.py &
+CUDA_VISIBLE_DEVICES=2 python held_suarez.py &
+CUDA_VISIBLE_DEVICES=3 python weather.py &
 wait

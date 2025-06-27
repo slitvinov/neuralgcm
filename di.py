fe9b34cff67b0e35c8df346662677ee42f48284e
Sergey Litvinov <slitvinov@gmail.com>
2025-06-27 13:50:16 +0200

    Revert "rm to_modal"
    This reverts commit bbbcaf8c10a978f3dec993036364a2558a029ba0.

--- di.py
+++ di.py
@@ -19,6 +19,16 @@ ideal_gas_constant = kappa * 0.0011628807950492582
 class g:
     pass
 
+
+def to_modal(z):
+
+    def g(x):
+        x = jnp.asarray(x)
+        return transform(x) if x.ndim else x
+
+    return tree_map(g, z)
+
+
 def transform(x):
     wx = g.w * x
     fwx = einsum("im,...ij->...mj", g.f, wx)

37006c8f2402a13133fc862e53cdccee92e76e8e
Sergey Litvinov <slitvinov@gmail.com>
2025-06-28 21:14:00 +0200

    up

--- di.py
+++ di.py
@@ -139,10 +139,10 @@ def runge_kutta(exp, imp, inv, dt):
     F = tree_math.unwrap(exp)
     G = tree_math.unwrap(imp)
     G_inv = tree_math.unwrap(inv, vector_argnums=0)
-    a_ex = [[1 / 3], [1 / 6, 1 / 2], [1 / 2, -1 / 2, 1]]
-    a_im = [[1 / 6, 1 / 6], [1 / 3, 0, 1 / 3], [3 / 8, 0, 3 / 8, 1 / 4]]
-    b_ex = [1 / 2, -1 / 2, 1, 0]
-    b_im = [3 / 8, 0, 3 / 8, 1 / 4]
+    a_ex = [1 / 3], [1 / 6, 1 / 2], [1 / 2, -1 / 2, 1]
+    a_im = [1 / 6, 1 / 6], [1 / 3, 0, 1 / 3], [3 / 8, 0, 3 / 8, 1 / 4]
+    b_ex = 1 / 2, -1 / 2, 1, 0
+    b_im = 3 / 8, 0, 3 / 8, 1 / 4
     n = len(b_ex)
 
     @tree_math.wrap

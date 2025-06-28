e03440258dc023c47063b067f2af561551574c69
Sergey Litvinov <slitvinov@gmail.com>
2025-06-29 01:03:26 +0200

    up

--- di.py
+++ di.py
@@ -84,11 +84,11 @@ def vadvection(w, x):
 
 def real_basis_derivative(u):
     n = 2 * g.longitude_wavenumbers - 1
-    i = np.mgrid[:n][:, None]
     y = jax.lax.slice_in_dim(u, 1, n, axis=-2)
     z = jax.lax.slice_in_dim(u, 0, n - 1, axis=-2)
     u_do = jax.lax.pad(y, 0.0, ((0, 0, 0), (0, 1, 0), (0, 0, 0)))
     u_up = jax.lax.pad(z, 0.0, ((0, 0, 0), (1, 0, 0), (0, 0, 0)))
+    i = np.c_[:n]
     return (i + 1) // 2 * jnp.where(i % 2, u_do, -u_up)
 
 

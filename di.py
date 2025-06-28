1d585b15f5eee14d20dcacc949464b12a63fdfc3
Sergey Litvinov <slitvinov@gmail.com>
2025-06-29 01:43:39 +0200

    up

--- di.py
+++ di.py
@@ -261,7 +261,7 @@ def explicit_terms(s):
     f_exp = jax.lax.cumsum(u_dot_grad * g.thick[:, None, None])
     f_full = jax.lax.cumsum((div + u_dot_grad) * g.thick[:, None, None])
     sum_sigma = np.cumsum(g.thick)[:, None, None]
-    sigma_dot = lambda f: (sum_sigma * f[-1:] - f)[:-1]
+    sigma_dot = lambda f: (sum_sigma * f[-1] - f)[:-1]
     sigma_exp = sigma_dot(f_exp)
     sigma_full = sigma_dot(f_full)
     sin_latitude, _ = scipy.special.roots_legendre(g.latitude_nodes)

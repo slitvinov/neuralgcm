dbb4ebc6cd12abc648b6dfc01930c3ed486db57b
Sergey Litvinov <slitvinov@gmail.com>
2025-06-29 17:26:07 +0200

    up

--- di.py
+++ di.py
@@ -76,7 +76,7 @@ def vadvection(w, x):
     wt = np.zeros((1, g.longitude_nodes, g.latitude_nodes))
     xt = np.zeros(shape)
     dx = x[1:] - x[:-1]
-    xd = einsum(dx, [0, 1, 2], 1 / g.center_to_center, [0], [0, 1, 2])
+    xd = dx / g.center_to_center
     wx = jnp.concatenate([wt, w, wt]) * jnp.concatenate([xt, xd, xt])
     return -0.5 * (wx[1:] + wx[:-1])
 

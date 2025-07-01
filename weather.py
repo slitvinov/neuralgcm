220e10c894803eb18222788e8794f184c9c8e4da
Sergey Litvinov <slitvinov@gmail.com>
2025-07-02 00:25:38 +0200

    Revert "up"
    This reverts commit e6bf89cb4eeffbb324532ace8fbcc6486f23d085.

--- weather.py
+++ weather.py
@@ -236,8 +236,17 @@ def G_inv(s, dt):
     row1 = np.c_[C, I, Z]
     row2 = np.c_[D, Z0, I0]
     inv = np.linalg.inv(np.r_['1', row0, row1, row2])
-    M = einsum("lgh,hml->gml", inv, s[g.ditesp])
-    return jnp.r_[s[g.vo], M, s[g.hu], s[g.wo], s[g.ic]]
+    di = (einsum("lgh,hml->gml", inv[:, :j, :j], s[g.di]) +
+          einsum("lgh,hml->gml", inv[:, :j, j:2 * j], s[g.te]) +
+          einsum("lgh,hml->gml", inv[:, :j, 2 * j:], s[g.sp]))
+    te = (einsum("lgh,hml->gml", inv[:, j:2 * j, :j], s[g.di]) +
+          einsum("lgh,hml->gml", inv[:, j:2 * j, j:2 * j], s[g.te]) +
+          einsum("lgh,hml->gml", inv[:, j:2 * j, 2 * j:], s[g.sp]))
+    sp = (einsum("lgh,hml->gml", inv[:, 2 * j:, :j], s[g.di]) +
+          einsum("lgh,hml->gml", inv[:, 2 * j:, j:2 * j], s[g.te]) +
+          einsum("lgh,hml->gml", inv[:, 2 * j:, 2 * j:], s[g.sp]))
+    return jnp.r_[s[g.vo], di, te, sp, s[g.hu], s[g.wo], s[g.ic]]
+
 
 def to_modal(z):
 

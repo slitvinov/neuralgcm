f93147db5d618af69f28b4866f640d3bca1b2ac9
Sergey Litvinov <slitvinov@gmail.com>
2025-07-01 23:30:50 +0200

    up

--- weather.py
+++ weather.py
@@ -448,7 +448,7 @@ divergence = div * mask
 
 temperature_variation = transform(t_nodal - g.temp.reshape(-1, 1, 1))
 log_sp = to_modal(np.log(sp_nodal))
-hu = to_modal(M["specific_humidity"])
+hu = transform(M["specific_humidity"])
 wo = transform(M["specific_cloud_liquid_water_content"])
 ic = transform(M["specific_cloud_ice_water_content"])
 raw_init_state = State(vorticity, divergence, temperature_variation, log_sp,

38def050ab8f4ae907162af73ce9fb68fa83a9d5
Sergey Litvinov <slitvinov@gmail.com>
2025-07-02 01:25:41 +0200

    up

--- weather.py
+++ weather.py
@@ -236,7 +236,7 @@ def G_inv(s, dt):
     row1 = np.c_[C, I, Z]
     row2 = np.c_[D, Z0, I0]
     inv = np.linalg.inv(np.r_['1', row0, row1, row2])
-    di = (einsum("lgh,hml->gml", inv[:, :j, :j], s[g.di]) +
+    di = (inv[:, :j, :j] @ s[g.di]) +
           einsum("lgh,hml->gml", inv[:, :j, j:2 * j], s[g.te]) +
           einsum("lgh,hml->gml", inv[:, :j, 2 * j:], s[g.sp]))
     te = (einsum("lgh,hml->gml", inv[:, j:2 * j, :j], s[g.di]) +

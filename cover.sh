6706fb7bd18455988a3ea8dbf122d5ed64ea2eed
Sergey Litvinov <slitvinov@gmail.com>
2025-07-01 19:55:58 +0200

    up

--- cover.sh
+++ cover.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TF_CPP_MIN_LOG_LEVEL=3 export TF_CPP_MIN_LOG_LEVEL
-CUDA_VISIBLE_DEVICES=3 COVERAGE_FILE=.coverage.2 python -m coverage run weather.py
+CUDA_VISIBLE_DEVICES=3 python -m coverage run weather.py
 
 python -m coverage combine
 python -m coverage html

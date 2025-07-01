ef807a3780f355b9ad164ab5afc4d41a948d66b1
Sergey Litvinov <slitvinov@gmail.com>
2025-07-01 19:53:02 +0200

    mv *.py to poc/

--- cover.sh
+++ cover.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 TF_CPP_MIN_LOG_LEVEL=3 export TF_CPP_MIN_LOG_LEVEL
-CUDA_VISIBLE_DEVICES=1 COVERAGE_FILE=.coverage.0 python -m coverage run baroclinic.py &
-CUDA_VISIBLE_DEVICES=2 COVERAGE_FILE=.coverage.1 python -m coverage run held_suarez.py &
-CUDA_VISIBLE_DEVICES=3 COVERAGE_FILE=.coverage.2 python -m coverage run weather.py &
-wait
+CUDA_VISIBLE_DEVICES=3 COVERAGE_FILE=.coverage.2 python -m coverage run weather.py
 
-python -m coverage combine &&
+python -m coverage combine
 python -m coverage html
--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 TF_CPP_MIN_LOG_LEVEL=3 export TF_CPP_MIN_LOG_LEVEL
-CUDA_VISIBLE_DEVICES=1 python baroclinic.py &
-CUDA_VISIBLE_DEVICES=2 python held_suarez.py &
-CUDA_VISIBLE_DEVICES=3 python weather.py &
-wait
+CUDA_VISIBLE_DEVICES=3 python weather.py

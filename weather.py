94724477f6f8ebcf07361fff458cdc81c58d9510
Sergey Litvinov <slitvinov@gmail.com>
2025-07-02 00:36:23 +0200

    up

--- weather.py
+++ weather.py
@@ -246,8 +246,8 @@ def G_inv(s, dt):
     sp = (einsum("lgh,hml->gml", inv[:, 2 * j:, :j], s[g.di]) +
           einsum("lgh,hml->gml", inv[:, 2 * j:, j:2 * j], s[g.te]) +
           einsum("lgh,hml->gml", inv[:, 2 * j:, 2 * j:], s[g.sp]))
-    jax.debug.pring(f"{jnp.min(jnp.diff(M - jnp.r_[di, te, sp]))}")
-    jax.debug.pring(f"{jnp.max(jnp.diff(M - jnp.r_[di, te, sp]))}")
+    jax.debug.print(f"{jnp.min(jnp.diff(M - jnp.r_[di, te, sp]))}")
+    jax.debug.print(f"{jnp.max(jnp.diff(M - jnp.r_[di, te, sp]))}")
     return jnp.r_[s[g.vo], di, te, sp, s[g.hu], s[g.wo], s[g.ic]]
 
 

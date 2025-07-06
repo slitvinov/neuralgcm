8b3fc3ac630d46774a57f78c40cf260187017ecd
Sergey Litvinov <slitvinov@gmail.com>
2025-07-06 15:42:51 +0200

    up

--- poc/diff.py
+++ poc/diff.py
@@ -4,4 +4,6 @@ import sys
 a = np.memmap(sys.argv[1], dtype=np.float32)
 b = np.memmap(sys.argv[2], dtype=np.float32)
 
-print(np.mean((a - b)**2))
+diff = (a - b)**2)
+
+print(np.min(diff), np.max(diff), np.mean(diff))

7efaf5e413509f04c72cbaa05a2ec295bc1482cd
Sergey Litvinov <slitvinov@gmail.com>
2025-07-07 14:06:43 +0200

    fori_loop

--- weather.py
+++ weather.py
@@ -368,8 +368,5 @@ else:
 g.dt = 4.3752000000000006e-02
 tau = 12900 / np.log2(g.ny / 128) / uT
 scale = jnp.exp(-g.dt * g.eig**2 / (tau * g.eig[-1]**2))
-out, *rest = jax.lax.scan(lambda x, _: (scale * runge_kutta(x), None),
-                          s,
-                          xs=None,
-                          length=579)
+out = lax.fori_loop(0, 579, lambda _, x: scale * runge_kutta(x), s)
 np.asarray(out).tofile("out.raw")

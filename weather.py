766239a6786f3ec978454b63b1e3702820953b68
Sergey Litvinov <slitvinov@gmail.com>
2025-07-07 11:01:08 +0200

    / g.nz

--- weather.py
+++ weather.py
@@ -288,7 +288,7 @@ p_alpha_shifted = np.roll(p_alpha, 1, axis=0)
 p_alpha_shifted[0] = 0
 h0 = kappa * g.temp * (p_alpha + p_alpha_shifted) * g.nz
 k1 = p - np.linspace(1/g.nz, 1, g.nz)
-g.tew = h0 * g.thick
+g.tew = h0 / g.nz
 g.l0 = np.r_[:g.l]
 g.eig = g.l0 * (g.l0 + 1)
 g.inv_eig = np.r_[0, -1 / g.eig[1:]]

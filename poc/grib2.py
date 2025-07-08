3500cda7e3be71759ca2cedc55a1cdabd570032f
Sergey Litvinov <slitvinov@gmail.com>
2025-07-08 07:45:37 +0200

    up

--- poc/grib2.py
+++ poc/grib2.py
@@ -14,13 +14,63 @@ def fma(x, y, z):
     return x * y + z
 
 
-CENTER = {98: "European Center for Medium-Range Weather Forecasts (RSMC)"}
+# https://www.nco.ncep.noaa.gov/pmb/docs/on388/table0.html
+CENTER = {
+    98: "European Center for Medium-Range Weather Forecasts (RSMC)",
+    00: "Melbourne (WMC)"
+}
+
 # https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table5-7.shtml
 PRECISION = {
     1: "IEEE 32-bit (I=4 in Section 7)",
     2: "IEEE 64-bit (I=8 in Section 7)",
     3: "IEEE 128-bit (I=16 in Section 7)"
 }
+
+# https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table0-0.shtml
+DISCIPLINE = {
+    0: "Meteorological Products",
+    1: "Hydrological Products",
+    2: "Land Surface Products",
+    3: "Satellite Remote Sensing Products",
+    4: "Space Weather Products",
+    10: "Oceanographic Products",
+    20: "Health and Socioeconomic Impacts",
+}
+
+# https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table1-0.shtml
+VERSION = {
+    0: "Experimental",
+    1: "7 November 2001",
+    2: "4 November 2003",
+    3: "2 November 2005",
+    4: "7 November 2007",
+    5: "4 November 2009",
+}
+
+# https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table1-2.shtml
+SIGNIFICANCE = {
+    0: "Analysis",
+    1: "Start of Forecast",
+    2: "Verifying Time of Forecast",
+    3: "Observation Time",
+    4: "Local Time",
+    5: "Simulation start",
+}
+
+# https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table1-3.shtml
+STATUS = {
+    0: "Operational Products",
+    1: "Operational Test Products",
+    2: "Research Products",
+}
+
+# https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table1-4.shtml
+TYPE = {
+    0: "Analysis Products",
+    1: "Forecast Products",
+}
+
 f = open(sys.argv[1], "rb")
 while True:
     # Section 0 - Indicator Section
@@ -28,21 +78,27 @@ while True:
     if section == b'':
         break
     assert section[:4] == b'GRIB'
-    assert section[6] == 0, "Meteorological Products"
+    discipline = section[6]
+    assert discipline == 0, "Discipline"
     assert section[7] == 2, "Edition"
     total_length, = struct.unpack(">Q", section[8:])
+    print(f"{DISCIPLINE[discipline]=}\n{total_length=}")
 
     # Section 1 - Identification Section
     section_length, = struct.unpack(">L", f.read(4))
     section = b'0000' + f.read(section_length - 4)
     assert section[4] == 1, "Number of the section"
     center, subcenter = struct.unpack(">HH", section[5:9])
-    assert section[11] == 1, "Start of Forecast"
+    version, local_version, significance = section[9:12]
     year, = struct.unpack(">H", section[12:14])
     month, day, hour, minute, second = section[15:20]
+    status, type_data = section[19:21]
 
-    print(f"{CENTER[center]=}")
+    print(f"{CENTER[center]=} {subcenter=}")
+    print(f"{VERSION[version]=} {local_version=}")
+    print(f"{SIGNIFICANCE[significance]=}")
     print(f"{year=} {month=} {day=} {minute=} {second=}")
+    print(f"{STATUS[status]=} {TYPE[type_data]=}")
 
     # Section 2 - Local Use Section
     section_length, = struct.unpack(">L", f.read(4))
@@ -55,7 +111,9 @@ while True:
     assert section[4] == 3, "Number of the section"
     assert section[5] == 0, "Source of Grid Definition"
     npoint, = struct.unpack(">L", section[6:10])
-    assert section[10] == 0, "number of octets for optional list is not zero"
+    noctet, interpetation = section[10:12]
+    print(f"{noctet=} {interpetation=}")
+    # assert noctet == 0 and interpetation == 0
 
     template_number, = struct.unpack(">H", section[12:14])
     assert template_number == 50, "Spherical harmonic coefficients"

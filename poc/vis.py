5ad16b21f927a7dd87a0bcb6ccc8c074d5e7d84e
Sergey Litvinov <slitvinov@gmail.com>
2025-07-08 18:01:23 +0200

    up

---
+++ poc/vis.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import math
+import scipy
+import re
+
+def nodal(x):
+    return einsum("im,mjl,...ml->...ij", g.f, g.p, x)
+
+class g:
+    pass
+
+einsum = np.einsum
+
+g.nx = 512
+g.nz = 32
+g.ny = 256
+g.m = 171
+g.l = 172
+g.f = np.empty((g.nx, 2 * g.m - 1))
+dft = scipy.linalg.dft(g.nx)[:, :g.m] / math.sqrt(math.pi)
+g.f[:, 0] = 1 / math.sqrt(2 * math.pi)
+g.f[:, 1::2] = np.real(dft[:, 1:])
+g.f[:, 2::2] = -np.imag(dft[:, 1:])
+g.sin_y, w = scipy.special.roots_legendre(g.ny)
+g.sec2 = 1 / (1 - g.sin_y**2)
+q = np.sqrt(1 - g.sin_y * g.sin_y)
+y = np.zeros((g.l, g.m, g.ny))
+y[0, 0] = 1 / np.sqrt(2)
+for m in range(1, g.m):
+    y[0, m] = -np.sqrt(1 + 1 / (2 * m)) * q * y[0, m - 1]
+for k in range(1, g.l):
+    fields = min(g.m, g.l - k)
+    m = np.c_[:fields]
+    m2 = m**2
+    mk2 = (m + k)**2
+    mkp2 = (m + k - 1)**2
+    a = np.sqrt((4 * mk2 - 1) / (mk2 - m2))
+    b = np.sqrt((mkp2 - m2) / (4 * mkp2 - 1))
+    y[k, :fields] = a * (g.sin_y * y[k - 1, :fields] - b * y[k - 2, :fields])
+r = np.transpose(y, (1, 2, 0))
+p = np.zeros((g.m, g.ny, g.l))
+for m in range(g.m):
+    p[m, :, m:g.l] = r[m, :, 0:g.l - m]
+p = np.repeat(p, 2, axis=0)
+g.p = p[1:]
+n = g.nz
+g.vo = np.s_[:n]
+g.di = np.s_[n:2 * n]
+g.te = np.s_[2 * n:3 * n]
+g.sp = np.s_[3 * n:3 * n + 1]
+g.hu = np.s_[3 * n + 1:4 * n + 1]
+g.wo = np.s_[4 * n + 1:5 * n + 1]
+g.ic = np.s_[5 * n + 1:6 * n + 1]
+g.ditesp = np.s_[n:3 * n + 1]
+shape = 6 * g.nz + 1, 2 * g.m - 1, g.l
+for path in sys.argv[1:]:
+    image = re.sub("[.]raw", "", path)
+    image = image + ".png"
+    s = np.fromfile(path, dtype=np.float32).reshape(shape)
+    sys.stderr.write(f"vis.py: {image}\n")
+    hu = nodal(s[g.hu])[g.nz//2]
+    vmin = np.min(hu)
+    vmax = np.max(hu)
+    plt.imsave(image, np.flipud(hu).T, cmap="jet", vmin=vmin, vmax=vmax)

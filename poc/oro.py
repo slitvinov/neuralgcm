703d320b8edb28c40cbf4746a5108af558644ced
Sergey Litvinov <slitvinov@gmail.com>
2025-07-10 14:31:32 +0200

    upd oro.png

--- poc/oro.py
+++ poc/oro.py
@@ -4,10 +4,12 @@ import numpy as np
 class g:
     pass
 
-g.nx = 512
-g.ny = 256
+g.m = 171
+g.l = g.m + 1
+g.nx = 3 * g.m + 1
+g.ny = g.nx // 2
 oro = np.memmap("oro.raw", dtype=float).reshape((g.nx, g.ny))
 oro = np.flipud(oro.T)
-oro = np.roll(oro, -g.nx//5, axis=1)
+# oro = np.roll(oro, -g.nx//5, axis=1)
 # oro = np.roll(oro, g.ny//4, axis=0)
 plt.imsave(f"oro.png", oro, cmap="jet")

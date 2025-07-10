f13e445cde66ed1795b47b9ff74737a8bf83d35f
Sergey Litvinov <slitvinov@gmail.com>
2025-07-11 00:05:52 +0200

    up

--- experiments/weather.sh
+++ experiments/weather.sh
@@ -5,7 +5,7 @@ set -e
 c=19900501T00
 while :
 do echo $c
-   c=$(date -d "${c:0:8} ${c:9:2}:00 +1 hour" '+%Y%m%dT%H')
+   c=$(LC_TIME=C date -d "${c:0:8} ${c:9:2}:00 +1 hour" '+%Y%m%dT%H')
 done | xargs -P 4 -n 2 --process-slot-var I sh -c '
      export XLA_PYTHON_CLIENT_PREALLOCATE=false
      export TF_CPP_MIN_LOG_LEVEL=3

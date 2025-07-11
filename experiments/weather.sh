8e8458bae5270ced7387381345d40f2c05471565
Sergey Litvinov <slitvinov@gmail.com>
2025-07-11 05:25:20 -0400

    u

--- experiments/weather.sh
+++ experiments/weather.sh
@@ -10,15 +10,17 @@ while True:
     print(c.strftime(fmt))
     c += timedelta(hours=1)
 ' | xargs -P 4 -n 2 --process-slot-var I sh -c '
+     dir=$0
+     set -- 0-15 16-31 32-48 49-63
      export XLA_PYTHON_CLIENT_PREALLOCATE=false
      export TF_CPP_MIN_LOG_LEVEL=3
      export CUDA_VISIBLE_DEVICES=$I
-     mkdir -p $0
-     cd $0
+     mkdir -p $dir
+     cd $dir
      date > start
-     python ../weather.py $0 2>stderr 1>stdout
+     python ../weather.py $dir 2>stderr 1>stdout
      date > end
      echo $? > status
-     python ../vis.py out.*.raw 2>stderr.vis 1>stdout.vis
+     python ../../poc/vis.py out.*.raw 2>stderr.vis 1>stdout.vis
      rm -rf *.raw
 '

8424ab202a6049dec86a42d8fa37aaad714af130
Sergey Litvinov <slitvinov@gmail.com>
2025-07-14 15:34:06 +0200

    run on two GPUs

--- experiments/weather.sh
+++ experiments/weather.sh
@@ -9,14 +9,20 @@ c = datetime.strptime("20000101T00", fmt)
 for i in range(50000):
     print(c.strftime(fmt))
     c += timedelta(hours=1)
-' | xargs -P 4 -n 1 --process-slot-var I sh -xc '
+' | xargs -P 2 -n 1 --process-slot-var I sh -xc '
      dir=$0
+
      set -- 0-31 32-63 64-95 96-127
      shift $I
      cpu=$1
+
+     set -- 2 3
+     shift $I
+     gpu=$1
+
      export XLA_PYTHON_CLIENT_PREALLOCATE=false
      export TF_CPP_MIN_LOG_LEVEL=3
-     export CUDA_VISIBLE_DEVICES=$I
+     export CUDA_VISIBLE_DEVICES=$gpu
      mkdir -p $dir
      cd $dir
      taskset -c $cpu python -u ../download.py $dir 2>stderr.download 1>stdout.download

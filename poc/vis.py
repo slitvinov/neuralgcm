32ff45312b8ac650714f22d6c1e4fbef755f275a
Sergey Litvinov <slitvinov@gmail.com>
2025-07-14 14:57:37 +0200

    dpi=600

--- poc/vis.py
+++ poc/vis.py
@@ -74,7 +74,7 @@ g.ic = np.s_[5 * n + 1:6 * n + 1]
 g.ditesp = np.s_[n:3 * n + 1]
 shape = 6 * g.nz + 1, 2 * g.m - 1, g.l
 
-fig = plt.figure(figsize=(4, 2), dpi=300)
+fig = plt.figure(figsize=(4, 2), dpi=600)
 ax = plt.axes(projection=cartopy.crs.PlateCarree(central_longitude=180))
 ax.add_feature(cartopy.feature.COASTLINE.with_scale('110m'), linewidth=0.3)
 ax.set_xticks([])
